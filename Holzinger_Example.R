3e1ad684a4cc497d19b7a0865de1d118264bc1a8
TheReturnOfTheSwarm <zulamman+1@gmail.com>
2025-12-18 00:56:31 +0100

    Add files via upload
    Adapted for the new Version

--- Holzinger_Example.R
+++ Holzinger_Example.R
@@ -7,12 +7,14 @@
   item_names <- colnames(HS_data[,7:30])
   HS_data <- as.data.frame(apply(HS_data[, item_names], 2, scale))
   
+  set.seed(456)
   
   # This is a small working example which will run several minutes (< 10 min.)
   # For more realistic settings of the hyperparameters please see text
-  max_iter <- 10     		 # number of iterations without improvement after which to abort search
-  n_bees <- 450     		 # number of n_bees
-  top_best <- 10      	 # how many solutions to use for onlookers
+  max_iter <- 5     		 # number of iterations without improvement after which to abort search
+  n_bees <- 200     		 # number of n_bees
+  nest_site_bees <- 200  # number of nest site bees for inital evaluation
+  top_best <- n_bees*0.2 # how many solutions to use for onlookers (for example 20% of n_bees)
   percent_scouts <- .25  # percentage scouts
   min_nest_fac <- 1    	 # how many correlated factors minimum
   max_nest_fac <- 5    	 # how many correlated factors maximum
@@ -31,7 +33,8 @@
           data = HS_data,
           max_iter = max_iter, 
           n_bees = n_bees,
-          n_start_bees = n_bees*3,
+          #n_start_bees = nest_site_bees,
+          nest_site_bees = nest_site_bees,
           percent_scouts = percent_scouts, 
           top_best = top_best, 
           min_nest_fac = min_nest_fac, 
@@ -40,25 +43,28 @@
           summaryfile = summaryfile,
           summaryfile_fin = summaryfile_fin,
           seed = seed,
-          verbose = FALSE,
-          parallel = F,
-          nCores = 16,
+          verbose = F,
+          parallel = T,
+          nCores = 8, # Define the number of available cores 
           plot_nectar = TRUE,
-          fit_crit = c("cfi", "rmsea",  "max_fac_cor1", "n_items", "residual"),
+          fit_crit = c("cfi", "rmsea",  "max_fac_cor1", "n_items","min_loading", "residual"),
           logistic_weights = list(c(d = 0.90, a = 30),
                                   c(d = 0.06, a = -50),
-                                  
-                                  c(d = 0.35, a = 15),
+                                  c(d = 0.80, a = -30),     # Max factor correlation
                                   c(d = 0.95, a = 50),
-                                  c(d = NA, a = NA)), #residual wont be logit transformed a second time
-          nu_weights = c(.5,.5,1,1,1),
-          nu_min = 0.0000000001,
+                                  c(d = 0.30, a = 50),
+                                  c(d = NA, a = NA)), #residual (local fit) wont be logit transformed a second time
+         # nu_weights = c(1,.5,1,1,1,1),
+          nu_weights = c(2,2,3,1,1,1), # Weighting matters, look at the comment examples below: This is close to the one in the paper
+          #nu_weights = c(2,2,3,2,1,1), # stronger weights n_items (less item elimination) --> weights influence results
+          nu_min = 0.00001,
           balance_n_fac = F,
-          n_start_items = 24,
-          bounds = "pos.var",
+          ignore_warnings  = F, # If True models which produce warnings won´t be penalized
+          bounds = "pos.var", # additional lavaan agrument 
           cluster_mode = cluster_mode,
           plot_list = list(xlim = c(0, max_iter*10), 
-                           ylim = c(0, sum(c(1,1,.5,1,1,1))),
+                           #ylim = c(0, sum(c(1,1,.5,1,1,1))),
+                           ylim = c(0, sum(c(2,2,3,1,1,1))),
                            ylab = "Overall Nectar Value",
                            xlab = "Iteration",
                            jitter_width = 0.5,
